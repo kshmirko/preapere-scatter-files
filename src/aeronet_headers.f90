154ffb465cc2c9a9aca3108c5684d22d712d5696
Dr. Konstantin Shmirko <kshmirko@gmail.com>
2021-10-28 00:20:24 +1000

    Небольшие правки в репо

--- src/aeronet_headers.f90
+++ src/aeronet_headers.f90
@@ -1,9 +1,76 @@
 module arnt_hdrs
 	
-	integer, parameter	::	AodExtTot_440nm = 10-3
-	integer, parameter	::	AodExtTot_675nm = 11-3
-	integer, parameter	::	AodExtTot_870nm = 12-3
-	integer, parameter	::	AodExtTot_1020nm =13-3
-
+	integer, parameter	::	AodExtTot_440nm = 1
+	integer, parameter	::	AodExtTot_675nm = 2
+	integer, parameter	::	AodExtTot_870nm = 3
+	integer, parameter	::	AodExtTot_1020nm =4
+	integer, parameter	::	AngExpTot		= 5
+	integer, parameter	::	AodAbsTot_440nm = 6
+	integer, parameter	::	AodAbsTot_675nm = 7
+	integer, parameter	::	AodAbsTot_870nm = 8
+	integer, parameter	::	AodAbsTot_1020nm =9
+	integer, parameter	::	RealMidx_440nm	= 10
+	integer, parameter	::	RealMidx_675nm	= 11
+	integer, parameter	::	RealMidx_870nm	= 12
+	integer, parameter	::	RealMidx_1020nm	= 13
+	integer, parameter	::	ImagMidx_440nm	= 14
+	integer, parameter	::	ImagMidx_675nm	= 15
+	integer, parameter	::	ImagMidx_870nm	= 16
+	integer, parameter	::	ImagMidx_1020nm	= 17
+	integer, parameter	::	Sphericity		= 18
+	integer, parameter	::	R01				= 19
+	integer, parameter	::	R02				= 20
+	integer, parameter	::	R03				= 21
+	integer, parameter	::	R04				= 22
+	integer, parameter	::	R05				= 23
+	integer, parameter	::	R06				= 24
+	integer, parameter	::	R07				= 25
+	integer, parameter	::	R08				= 26
+	integer, parameter	::	R09				= 27
+	integer, parameter	::	R10				= 28
+	integer, parameter	::	R11				= 29
+	integer, parameter	::	R12				= 30
+	integer, parameter	::	R13				= 31
+	integer, parameter	::	R14				= 32
+	integer, parameter	::	R15				= 33
+	integer, parameter	::	R16				= 34
+	integer, parameter	::	R17				= 35
+	integer, parameter	::	R18				= 36
+	integer, parameter	::	R19				= 37
+	integer, parameter	::	R20				= 38
+	integer, parameter	::	R21				= 39
+	integer, parameter	::	R22				= 40
+	integer, parameter	::	VolCT			= 41
+	integer, parameter	::	REffT			= 42
+	integer, parameter	::	VMRT			= 43
+	integer, parameter	::	StdT			= 44
+	integer, parameter	::	VolCF			= 45
+	integer, parameter	::	REffF			= 46
+	integer, parameter	::	VMRF			= 47
+	integer, parameter	::	StdF			= 48
+	integer, parameter	::	VolCC			= 49
+	integer, parameter	::	REffC			= 50
+	integer, parameter	::	VMRC			= 51
+	integer, parameter	::	StdC			= 52
+	integer, parameter	::	LR_440nm		= 53
+	integer, parameter	::	LR_675nm		= 54
+	integer, parameter	::	LR_870nm		= 55
+	integer, parameter	::	LR_1020nm		= 56
+	integer, parameter	::	DLR_440nm		= 57
+	integer, parameter	::	DLR_675nm		= 58
+	integer, parameter	::	DLR_870nm		= 59
+	integer, parameter	::	DLR_1020nm		= 60
+	integer, parameter	::	SufAlbedo_440nm = 61
+	integer, parameter	::	SufAlbedo_675nm = 62
+	integer, parameter	::	SufAlbedo_870nm = 63
+	integer, parameter	::	SufAlbedo_1020nm = 64
+	integer, parameter	::	USED_COLUMNS_COUNT = 64
+	integer, parameter	::	COLUMNS_COUNT 	= 155
+	integer, parameter	::	indices(USED_COLUMNS_COUNT)		= &
+	&[ 11, 12, 13, 14, 23, 28, 29, 30, 31, 33, 34, 35, 36, 37,&
+	& 38, 39, 40, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, &
+	& 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 77, 78, &
+	& 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 113, 114, 115,  &
+	& 116, 117, 118, 119, 120, 142, 143, 144, 145 ]
 	
 end module arnt_hdrs
--- src/main.f90
+++ src/main.f90
@@ -4,18 +4,21 @@ program main
 	use arnt_hdrs
 	implicit none
 	integer, parameter	::	dp = kind(1.d0)
-	integer, parameter	::	MAX_RECS = 141
+	
 	integer(kind=kind(10))	::	I,J, ERR, unit, N, doy
 	character(len=20)		::	site, date, time
 	character(len=80)		::	errmsg
-	real(kind=dp)			::	row(MAX_RECS)
+	character(len=15)		::	raw_row(COLUMNS_COUNT)
+	real(kind=dp)			::	used_row(USED_COLUMNS_COUNT)
+	logical 				::	error_read_line, &
+								&error_parse_line
 	N=10
 
-	
-	
-	open(newunit=unit, file='data.csv', status='old', iostat=ERR)
+	open(newunit=unit, file='data.csv', status='old', iostat=ERR,&
+		& iomsg=errmsg)
 	if (ERR.ne.0) then
-    	print *, 'Error opening file'
+    	
+		print *, errmsg
     	stop
     endif
 
@@ -23,34 +26,64 @@ program main
     read(unit, *)
 	
 	I=0
-	
+	error_parse_line = .FALSE.
+	error_read_line = .FALSE.
 mainloop: DO 
-		dat = 0.0
-    	read(unit, *, iostat=ERR, iomsg=errmsg) site, date, time, doy, &
-		& (dat(J),j=1, MAX_RECS)
+		! очищаем память под массив данных
+		raw_row = ''
+		! читаем строку из файла в массив строк
+    	read(unit, *, iostat=ERR, iomsg=errmsg) &
+			&(raw_row(J),J=1,COLUMNS_COUNT)
+		
+		! Если при чтении возникла ошибка, 
 		if (ERR.ne.0) then
     		print '(A, I5)', 'Error=', ERR
 			print '(A)', errmsg
+			error_read_line = .TRUE.
 			exit mainloop
 		end if
 		
-		i=i+1
+		I=I+1 ! увеличиваем счетчик прочитанных строк
+		
+		! Для отладочных целей, в продакшене нужно убрать этот блок
 		if (I .gt. 10) then
 			exit mainloop
 		end if
 		
-		print '(3A15, 5F5.2)', site, date, time,&
-		&row(AodExtTot_440nm:AodExtTot_1020nm)
+		!разбираем интересующие нас столбцы
+		inner_loop: DO J=1, USED_COLUMNS_COUNT
+			read(raw_row(indices(J)), *, iostat=ERR, iomsg=errmsg)&
+			& used_row(J)
+			
+			
+			if (ERR.ne.0) then
+	    		print '(A, I5)', 'Error=', ERR
+				print '(A)', errmsg
+				error_parse_line=.TRUE.
+				exit mainloop
+			end if
+			
+		END DO inner_loop
+		
+		if (.not. error_parse_line) then
+		end if
+		
+		!print '(4A10)', raw_row(indices(1):indices(4))
+		!print '(4F7.2)', used_row(1:4)
     END DO mainloop
 
     close(unit)
 
-    call DLS_read_input("input_sphrs.dat")
-	call alloc_dls_array(key, keyel, 1)
+	
+	if (.not. (error_read_line .and. error_parse_line)) then
+	    call DLS_read_input("input_sphrs.dat")
+		call alloc_dls_array(key, keyel, 1)
 
-	call optchar(ndp)
+		call optchar(ndp)
 
-	call alloc_dls_array(key, keyel, 2)
+		call alloc_dls_array(key, keyel, 2)
+	end if
+    
 	
 end program main
 
